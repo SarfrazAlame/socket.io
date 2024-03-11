c1151f46a4972aaa30230a99c5d825d492672592
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-03-11 19:09:16 +0530

    dljkhfgds

---
+++ serverwithjs/app.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import cors from 'cors'
+
+const port = 4000;
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
+});
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+io.on("connection", (socket) => {
+    console.log("User connected")
+    console.log("Id", socket.id)
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
